05ad2c93fbf658f92fabc3def1789cd04d522081
Marcus <marcus.gamboa@gmail.com>
2021-11-12 08:14:11 -0700

    Add connection file

--- smilga/starter/app.js
+++ smilga/starter/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
-const app = express();
+const connectDB = require("./db/connect");
 
-const uri = process.env.URI;
+const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 8000;
+const uri = process.env.MONGO_URI;
 
 // middleware
 app.use(express.json());
@@ -15,6 +17,19 @@ app.get("/hello", (req, res) => {
   res.send("Task Manger");
 });
 
-app.listen(port, () =>
-  console.log(`Server is listening on port ${port} (http://localhost:8000/)`)
-);
+// Server/DB Connection
+
+const start = async () => {
+  try {
+    await connectDB(uri);
+    app.listen(port, () =>
+      console.log(
+        `Server is listening on port ${port} (http://localhost:8000/)`
+      )
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
